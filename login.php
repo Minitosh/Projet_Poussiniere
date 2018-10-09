d2ccdee38c46b35772cb86be623e3947809cf2a7
Marc Deshayes <33457027+Minitosh@users.noreply.github.com>
2018-10-09 18:27:05 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+	<title>Application</title>
+</head>
+<body>
+    <div class="body">
+        <img class="logo" src="images/icons/french-upers-logo-jaune-poussin.png" alt="" />
+        
+        <h1 class="h1">LOGIN</h1>
+        
+    </div>
+    <div class="textbox">
+        <input type ="text" name="utilisateur" placeholder="utilisateur">
+        <br>
+        <br>
+        <input type ="password" name="mot de passe" placeholder="mot de passe">
+        <br>
+    </div>
+    
+    <div class="loginbutton">
+        <input type="button" value="LOGIN">
+        <a href="index.php" class="bouton-relief">Accueil</a>
+        
+    </div>
+    
+</body>
+</html>
