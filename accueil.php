f9634774df7776863ab80aba04ebe13609cfc997
Marc Deshayes <33457027+Minitosh@users.noreply.github.com>
2018-10-09 18:24:53 +0200

    Create accueil.php

---
+++ accueil.php
@@ -0,0 +1,16 @@
+<!--Afichage de l'horloge-->
+<div class="clock" id="horloge">XX:XX</div>
+ 
+<!--Description du site web-->
+    <div class="cadre">
+        <h2>    <center>
+                    <img src="images/icons/french-upers-logo-jaune-poussin.png" width="100" height="100"/>
+                </center><br>
+                Bonjour et bienvenue sur ce site web.<br>
+                Graçe à celui çi, vous allez pouvoir gerer differents paramètres au sein de la poussiniere.<br><br>
+                Dans l'onglet "RELEVES", vous pourrez consulter l'affichage des températures actuelles dans la poussinière, du niveau de l'eau, de l'historique des différents relevés des capteurs et de l'historique des intrusions.<br><br>
+                Dans l'onglet "CONSIGNES", vous pourrez définir différents paramètres tels que le choix de la température de l'air et le choix de la température de l'eau.
+                Il vous indique aussi les normes de nourriture pour la croissance des poussins.<br><br>
+                Dans l'onglet "AUTRES", vous trouverez les informations relatives à la création de ce site web.
+            </h2>
+    </div>
