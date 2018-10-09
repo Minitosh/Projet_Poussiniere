88d337b793f270ed5037b9426c478f65c9a90581
Marc Deshayes <33457027+Minitosh@users.noreply.github.com>
2018-10-09 18:36:06 +0200

    Create consignes_nourriture.php

---
+++ consignes_nourriture.php
@@ -0,0 +1,15 @@
+<!--Redirection vers les pages choisies-->
+<div class="navbar">
+       <a href="index.php?page=consignes" class="navbar ongletbotOFF">Température Air</a>
+       <a href="index.php?page=consignes_tempeau" class="navbar ongletbotOFF">Température Eau</a>
+       <a href="index.php?page=consignes_nourriture" class="navbar ongletbotON">Nourriture</a>
+</div>
+
+<!--Affichage du tableau des normes de dose de nourriture-->
+<div class="cadrefonction">
+    <br><br><br><br><br><br><br>
+    <center>
+        <img src="images/icons/normesnourriture.png" width="550" height="300"/>
+    </center>
+    
+</div>
